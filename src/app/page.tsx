348981e25a56c16565b84a24f255c2faf32b6866
nlucis <arjay.jones@nlucis.dev>
2024-12-15 15:57:10 -0800

    mapbox globe added with base styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,71 @@
 'use client';
-
+import React, { 
+  useRef,
+  useEffect,
+  // useState,
+} from 'react';
+import Mapbox, { 
+  GeolocateControlOptions, 
+  NavigationControlOptions 
+} from 'mapbox-gl';
 import { NextPage } from 'next';
+import { MAP } from 'res/lib/constants';
 
 
 const App: NextPage = () => {
 
   // Viewer State
-  return <>
-    <div id='map-container' className='w-full h-full fixed'></div>
-  </>;
+  const _mapContainer = useRef<HTMLDivElement>(null);
+
+  // Initialize Map
+  useEffect(() => {
+    if (_mapContainer.current) {
+      const map = new Mapbox.Map({
+        container: _mapContainer.current,
+        style: MAP.ZON,
+        projection: 'globe',
+        attributionControl: false,
+        // preserveDrawingBuffer: true,
+        hash: true,
+        accessToken: process.env['NEXT_PUBLIC_MAPBOX_TOKEN'],
+        antialias: true,
+        
+      });
+
+      // Map Controls Config
+      const geoControls = new Mapbox.GeolocateControl({
+        showUserHeading: true,
+        showUserLocation: true,
+        trackUserLocation: true,
+        showAccuracyCircle: true,
+        positionOptions: { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 },
+        fitBoundsOptions: { maxZoom: 18, duration: 1000 },
+      } as GeolocateControlOptions);
+
+      if ('navigator' in window) {  
+        geoControls.options.geolocation = navigator.geolocation;
+      }
+
+      const mapControls = new Mapbox.NavigationControl({
+        showCompass: true,
+        showZoom: true,
+        visualizePitch: true,
+      } as NavigationControlOptions);
+
+      const fullscreenToggle = new Mapbox.FullscreenControl({
+        container: document.getElementById('app') as HTMLElement,
+      });
+
+      // Mapbox Controls - Order Sensitive
+      map.addControl(geoControls, 'top-right');
+      map.addControl(mapControls, 'top-right');
+      map.addControl(fullscreenToggle, 'top-right');
+      return () =>  map.remove();  
+    }
+    
+  }, []);
+
+  return <div ref={ _mapContainer } style={{ width: '100vw', height: '100vh' }}></div>
 }
 
 export default App;

98a358beab1e45a02c4ce3bfbbf85e12ee365ad6
nlucis <arjay.jones@nlucis.dev>
2024-12-15 16:03:17 -0800

    done.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+/* All Globally Re-Usable Variables */
 export const MAP = {
   ZON: 'mapbox://styles/arjaycodes/cm0viukep00wa01pw2iwq147c',
   ZON_SAT: 'mapbox://styles/arjaycodes/cm1cw2me9032h01rbfsjahqqa',
